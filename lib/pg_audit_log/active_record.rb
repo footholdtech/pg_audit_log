b329a816afa476e4b001a02df6095639aee1702c
Wilson Fong <wfong@footholdtechnology.com>
2025-08-28 16:02:30 -0400

    Allow configurable DB connection class

--- lib/pg_audit_log/active_record.rb
+++ lib/pg_audit_log/active_record.rb
@@ -1,10 +1,20 @@
 module PgAuditLog
   class ActiveRecord
     class << self
+      @@audit_classname = 'ActiveRecord::Base'
+
+      def audit_classname
+        @@audit_classname
+      end
+
+      def audit_classname=(classname)
+        @@audit_classname = classname
+      end
+
       private
 
       def connection
-        ::ActiveRecord::Base.connection
+        audit_classname.constantize.connection
       end
 
       def execute(sql)
